e903c2c9c8b76827661b1f5022e8ef6800fc5ef7
KevinArmache <kevinarmache@gmail.com>
2023-01-06 12:42:03 +0100

    add: organize main

--- components/elements/basic/Logo.js
+++ components/elements/basic/Logo.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-const Logo = ({ url = '/', light }) => {
-    if (light) {
-        return (
-            <Link href={url}>
-                <a className="ps-logo">
-                    <img src="/static/img/logo-white.png" alt="" />
-                </a>
-            </Link>
-        );
-    } else {
-        return (
-            <Link href={url}>
-                <a className="ps-logo">
-                    <img src="/static/img/logo.svg" alt="" />
-                </a>
-            </Link>
-        );
-    }
+const Logo = ({ url = "/", light }) => {
+  if (light) {
+    return (
+      <Link href={url}>
+        <a className="ps-logo">
+          {/* <img src="/static/img/logo-white.png" alt="" /> */}
+          <img src="../../../assets/logo.jpeg" alt="" />
+        </a>
+      </Link>
+    );
+  } else {
+    return (
+      <Link href={url}>
+        <a className="ps-logo">
+          {/* <img src="/static/img/logo.svg" alt="" /> */}
+          <img src="../../../assets/logo.jpeg" alt="" />
+        </a>
+      </Link>
+    );
+  }
 };
 
 export default Logo;
